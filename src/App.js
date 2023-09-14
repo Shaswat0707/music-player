c889583f8aed96034f7a98326dac5b7c57d961d9
Shaswat0707 <shaswatvaidya@gmail.com>
2023-09-15 02:40:30 +0530

    song and player done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import Player from './components/Player'
-import Song from './components/Song';
+import { useState } from "react";
+import Player from "./components/Player";
+import Song from "./components/Song";
 import "./styles/app.scss";
+import data from "./util";
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
